fcbe73ce5c53d179b103b3a75231bc02f2e9e988
Kiera <kieracrawford2005@gmail.com>
2025-10-10 12:55:01 -0600

    Remote start code started

---
+++ Demo_1/Computer_Vision/remoteStart.py
@@ -0,0 +1,37 @@
+from time import sleep
+from smbus2 import SMBus, i2c_msg
+import struct
+
+TARGET_DIST = 5.5     # in feet
+TARGET_ANG = -45.25   # in degrees
+
+# Pack floats into bytes (little-endian)
+dist_bytes = struct.pack('<f', TARGET_DIST)
+ang_bytes = struct.pack('<f', TARGET_ANG)
+send = list(dist_bytes + ang_bytes)
+
+ARD = 0x08  # Arduino I2C address
+
+with SMBus(1) as i2c:
+    for i in range(20):
+        try:
+            # Send 8 bytes: 4 for distance, 4 for angle
+            msg = i2c_msg.write(ARD, send)
+            i2c.i2c_rdwr(msg)
+            sleep(1)
+
+            # Read 8-byte reply
+            reply = i2c_msg.read(ARD, 8)
+            i2c.i2c_rdwr(reply)
+            check = list(reply)
+
+            # Unpack floats
+            dist_reply = struct.unpack('<f', bytes(check[0:4]))[0]
+            ang_reply = struct.unpack('<f', bytes(check[4:8]))[0]
+
+            print(f"Arduino confirmed: {dist_reply:.2f} ft, {ang_reply:.2f}°")
+            break
+
+        except (IOError, OSError):
+            print("I2C communication error.")
+            sleep(0.1)
---
+++ Demo_1/remoteStart.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+
+#define MY_ADDR 0x08
+
+float received_distance = 0.0;
+float received_rotation = 0.0;
+bool received = false;
+
+void setup() {
+  Wire.begin(MY_ADDR);
+  Wire.onReceive(onReceiveEvent);
+  Wire.onRequest(onRequestEvent);
+  Serial.begin(115200);
+  Serial.println("Ready to receive float motion commands.");
+}
+
+void loop() {
+  if (received) {
+    Serial.print("Received Distance (ft): ");
+    Serial.print(received_distance, 2);
+    Serial.print(" | Rotation (deg): ");
+    Serial.println(received_rotation, 2);
+    received = false;
+  }
+}
+
+void onReceiveEvent(int numBytes) {
+  if (numBytes >= 8) {
+    union { byte b[4]; float f; } dist, rot;
+    for (int i = 0; i < 4; i++) dist.b[i] = Wire.read();
+    for (int i = 0; i < 4; i++) rot.b[i] = Wire.read();
+    received_distance = dist.f;
+    received_rotation = rot.f;
+    received = true;
+  }
+}
+
+void onRequestEvent() {
+  union { byte b[4]; float f; } dist, rot;
+  dist.f = received_distance;
+  rot.f = received_rotation;
+  for (int i = 0; i < 4; i++) Wire.write(dist.b[i]);
+  for (int i = 0; i < 4; i++) Wire.write(rot.b[i]);
+}
