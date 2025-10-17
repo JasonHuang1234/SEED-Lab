089ac684e6ea2a5b5ceb47f01604778967ffc2af
Jason Huang <huangjason1229@gmail.com>
2025-10-17 12:08:20 -0600

    syntax

--- Demo_1/Computer_Vision/main.py
+++ Demo_1/Computer_Vision/main.py
@@ -81,8 +81,8 @@ while True:
         #The below code is more accurate but much slower
         #rvec, tvec, _ = cv.aruco.estimatePoseSinglemarkers(marker_corners,marker_length,mtx,dist )
         #cv.drawFrameAxes(frame,mtx,dist,rvec,tvec,0.03)
-        x = tvec[0][0][0]
-        z = tvec[0][0][2]
+        #x = tvec[0][0][0]
+        #z = tvec[0][0][2]
         angle = np.arctan(xcenter-cx,fx)
         angle = np.rad2deg(angle)
         angle = np.round(angle,4)
