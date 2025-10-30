ac23dd0638747853acca2783665d052304a528d6
Cameron Alcorn <cjalcorn@mines.edu>
2025-10-29 19:51:52 -0600

    Added final version of Demo1LC with comments

--- Demo_1/Controls/Demo1LC.ino
+++ Demo_1/Controls/Demo1LC.ino
@@ -1,9 +1,3 @@
-// Main Demo 1 Sketch
-// Primary developers: Stephanie and Cameron
-// MM/DD/2025
-// Description: Navigates robot to a travel a target distance
-// or rotate a set number of degrees
-
 #define Motor_Enable 4
 #define M1Voltage_Sign 7
 #define M2Voltage_Sign 8
@@ -30,8 +24,8 @@ K2 = 1.5  sigma2 = 14.5
 */
 
 const float pi = 3.1415926538;      // pi with 8 decimal places
-const float R = 0.074;              // Wheel radius in meters
-const float wheel_base = 0.3556;    // Distance between wheels in meters
+const float R = 0.075;              // Wheel radius in meters
+const float wheel_base = 0.355;    // Distance between wheels in meters (0.3556)
 const int counts_per_rev = 3200; //number of encoder counts per full wheel revolution
 const float BATTERY_VOLTAGE = 7.8;
 const float ANGLE_TOLERANCE = 0.75 * (pi/180); // 0.75 degrees in radians
@@ -40,9 +34,9 @@ float applied_voltage[2] = {0,0};
 float PWM[2];
 
 // Define gain, speed, and error variables
-float Kp_vel[2] = {2,2};
-float Kp_pos[2] = {15,15};
-float Ki_pos[2] = {3,3};
+float Kp_vel[2] = {3,3.2};
+float Kp_pos[2] = {4,4};
+float Ki_pos[2] = {0.05,0.05};
 
 float desired_pos_xy[2] = {0,0}; // {angle (degrees), distance (meters)}
 bool angle_set = false; // Checks whether angle has been set and robot should move forward
@@ -67,12 +61,20 @@ volatile long int Enc_Counter[2] = {0,0};
 float posX = 0.0, posY = 0.0, phi = 0.0;
 float prev_rad[2] = {0, 0}; // Previous wheel angles (radians)
 
+float angle_position_0 = actual_pos[0];
+float angle_position_1 = actual_pos[1];
+
 // Define time keeping variables
 unsigned long desired_Ts_ms = 10; // desired sample time in milliseconds
 unsigned long last_time_ms;
 unsigned long start_time_ms;
 float current_time;
 bool finished = false; // Define bool to use to print finished
+
+float angle_set_time = 0;
+
+const float ANGLE = 0;
+const float DISTANCE = 8;
 
 
 
@@ -109,31 +111,34 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(M1EncA), M1Enc_Update, CHANGE);
   attachInterrupt(digitalPinToInterrupt(M2EncA), M2Enc_Update, CHANGE);
 
-  // Print ready for MATLAB
-  // Serial.println("Ready!");
-
 }
 
 void loop() {
 
-    if (received){ // This doesnt really do anything but print random stuff you should put your code in this if statement, 
-      noInterrupts();
-      Serial.print("Received: ");
-      Serial.print(desired_pos_r[0]);
-      Serial.print(", ");
-      Serial.println(desired_pos_r[1]);
-      interrupts();
-      received = false;
+    if (received){ // Code to recieve instructions from pi
+    noInterrupts();
+    Serial.print("Received: ");
+    Serial.print(desired_pos_r[0]);
+    Serial.print(", ");
+    Serial.println(desired_pos_r[1]);
+    interrupts();
+    received = false;
     }
 
     // Change desired position at 5 seconds
-    if (current_time >= 5 && !finished) {
-      desired_pos_xy[0] = 90;
-      desired_pos_xy[1] = 0;
-      angle_set = false;
-      angle_changed = true;
-      distance_changed = true;
-      finished = true;
+    if (current_time >= 5) {
+      if (abs(desired_pos_xy[0]) < abs(ANGLE)) {
+        desired_pos_xy[0] += constrain(ANGLE*0.005,-0.36,0.36); // Slowly increase desired position to prevent high acceleration
+        desired_pos_xy[0] = constrain(desired_pos_xy[0],-abs(ANGLE),abs(ANGLE));
+        angle_changed = true;
+        angle_set = false;
+      }
+      
+      if (desired_pos_xy[1] < DISTANCE*(0.3048) && angle_set && (current_time > (angle_set_time + 3) ) ) {
+        desired_pos_xy[1] += DISTANCE*(0.3048)*0.001; // Slowly increase desired position to prevent high acceleration
+        desired_pos_xy[1] = constrain(desired_pos_xy[1],0,DISTANCE*(0.3048));
+        distance_changed = true;
+      }
     }
 
     // Move robot to desired angle
@@ -141,38 +146,26 @@ void loop() {
 
       if (angle_changed) {
         // Calculations to convert desired robot angle to desired wheel angle
-        desired_pos_r[0] += -( (desired_pos_xy[0]*(pi/180)) * wheel_base ) / (2*R);
-        desired_pos_r[1] += ( (desired_pos_xy[0]*(pi/180)) * wheel_base ) / (2*R);
+        desired_pos_r[0] = -( (desired_pos_xy[0]*(pi/180)) * wheel_base ) / (2*R);
+        desired_pos_r[1] = ( (desired_pos_xy[0]*(pi/180)) * wheel_base ) / (2*R);
         angle_changed = false;
       }
-
-      // Testing
-      Serial.print("X Position: ");
-      Serial.print(posX);
-      Serial.print("\t");
-      Serial.print("Y Position: ");
-      Serial.print(posY);
-      Serial.print("\t");
-      Serial.print("Phi: ");
-      Serial.print(phi);
-      Serial.print("\t");
-      Serial.print("Distance Travelled: ");
-      Serial.println(sqrt( posX^2 + posY^2 ));
 
       // If the robots angle is within the desired tolerance, allow robot to move desired distance
       if ( (abs(phi - (desired_pos_xy[0]*(pi/180))) < ANGLE_TOLERANCE) && actual_vel[0] <  0.1) {
         angle_set = true;
         Serial.println("Angle Set");
+        angle_position_0 = actual_pos[0];
+        angle_position_1 = actual_pos[1];
+
+        angle_set_time = millis() / 1000;
       }
 
     } else if (angle_set) { 
 
-      if (distance_changed) {
-        // calculations to convert desired robot distance to desired wheel angle
-        desired_pos_r[0] += desired_pos_xy[1] / R;
-        desired_pos_r[1] += desired_pos_xy[1] / R;
-        distance_changed = false;
-      }
+      // calculations to convert desired robot distance to desired wheel angle
+      desired_pos_r[0] = (desired_pos_xy[1] / R) + angle_position_0;
+      desired_pos_r[1] = (desired_pos_xy[1] / R) + angle_position_1;
 
     }
 
@@ -194,19 +187,25 @@ void loop() {
 
       // Calculate position and integral errors
       pos_error[i] = desired_pos_r[i] - actual_pos[i];
-      
       integral_error[i] = integral_error[i] + pos_error[i]*(dt);
-      integral_error[i] = constrain(integral_error[i], -10.0, 10.0); // CHANGE CONSTRAINTS
+      integral_error[i] = constrain(integral_error[i], -50.0, 50.0); // Constrain integral error
 
-      // Calculate desired velocity and velocity error 
-      desired_vel[i] = constrain(Kp_pos[i] * pos_error[i] + Ki_pos[i] * integral_error[i], -10, 10); // Constrain maximum desired veolcity to prevent slipping?
+      // Calculate desired velocity for each motor
+      desired_vel[i] = Kp_pos[i] * pos_error[i] + Ki_pos[i] * integral_error[i]; // Constrain maximum desired veolcity to prevent slipping?
+
+      // Add error to correct for drifting
+      float heading_error = desired_pos_xy[0]*(pi/180) - phi;
+      float heading_correction = 5 * heading_error;
+      desired_vel[0] -= heading_correction;
+      desired_vel[1] += heading_correction;
+
+      // Calculate velocity error and applied voltage
       vel_error[i] = desired_vel[i] - actual_vel[i];
-
-      // Calculate applied voltage
       applied_voltage[i] = Kp_vel[i]*vel_error[i];
 
       // Calculate PWM signal 
-      PWM[i] = constrain( (abs(applied_voltage[i]) / BATTERY_VOLTAGE) * 255, 0, 255);
+      PWM[0] = constrain( (abs(applied_voltage[0]) / BATTERY_VOLTAGE) * 255, 0, 180); // 180
+      PWM[1] = constrain( (abs(applied_voltage[1]) / BATTERY_VOLTAGE) * 255, 0, 189); // 189
       
       // Save for next iteration
       last_pos[i] = actual_pos[i];
@@ -232,44 +231,20 @@ void loop() {
     float delta_center = (delta_M[0] + delta_M[1]) / 2.0;
     phi += (delta_M[1] - delta_M[0]) / wheel_base;
 
+    /*
     // Normalize phi
     if (phi > pi) phi -= 2 * pi;
     if (phi < -pi) phi += 2 * pi;
+    */
 
     posX += cos(phi) * delta_center;
     posY += sin(phi) * delta_center;
-
-    /*
-    // Record data
-    if (current_time <= 100) {
-      // Print time, voltage, position, and velocity
-      Serial.print(current_time);
-      Serial.print("\t");
-      Serial.print(constrain(applied_voltage[0],-7.5,7.5));
-      Serial.print("\t");
-      Serial.print(actual_pos[0]);
-      Serial.print("\t");
-      Serial.print(actual_vel[0]);
-      Serial.print("\t");
-      Serial.print(posX); 
-      Serial.print("\t");
-      Serial.print(posY); 
-      Serial.print("\t");
-      Serial.println(phi*180/pi);
-    } else if (current_time > 100) { 
-      // Print finished for MATLAB
-      if (!finished) {
-        Serial.println("Finished");
-      }
-      finished = true;
-    }
-    */
 
     while (millis()<last_time_ms + desired_Ts_ms) {
     // Wait until desired time passes to go top of the loop
     } //Line at top replaces this??
 
-    last_time_ms = now;  
+    last_time_ms = now;
 
     // Uncomment to test remote start
     // if (received) {
@@ -323,19 +298,17 @@ void M2Enc_Update() { // Interrupt function for motor 2's encoder
 }
 
 
+void onReceiveEvent() {
+  int i = 0;
+  while (Wire.available() && i < 2){
+    desired_pos_r[i] = Wire.read();
+    return_vals[i] = desired_pos_r[i];
+    i++;
+  }
+  received = true;
+}
 
-//Old comms code for new comms code uncomment lines 13, 268-288
-// void onReceiveEvent() {
-//   int i = 0;
-//   while (Wire.available() && i < 2){
-//     desired_pos_r[i] = Wire.read();
-//     return_vals[i] = desired_pos_r[i];
-//     i++;
-//   }
-//   received = true;
-// }
-
-// // This on a request event writes to the pi
-// void onRequestEvent() {
-//   Wire.write((const uint8_t*)return_vals, 2);        // send one byte
-// }
+// This on a request event writes to the pi
+void onRequestEvent() {
+  Wire.write((const uint8_t*)return_vals, 2);        // send one byte
+}
