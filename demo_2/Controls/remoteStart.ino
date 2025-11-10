ff6c84d92289a1cf630b0952219c0cfd4227f9f6
Kiera <kieracrawford2005@gmail.com>
2025-11-10 12:09:49 -0700

    Fix remote start cpp to match ino

--- demo_2/Controls/remoteStart.ino
+++ demo_2/Controls/remoteStart.ino
@@ -1,3 +1,9 @@
+// Remote Start Arduino Code
+// Primary developer: Kiera Crawford
+// Updated: 10/22/2025
+// Description: This is a helper file that just does the comms part of Arduino tasks
+// This is a comms testing file but the code can be copied to match functionalty in main sketch
+
 #include <Wire.h>
 
 #define MY_ADDR 0x08
@@ -25,13 +31,57 @@ void loop() {
 }
 
 void onReceiveEvent(int numBytes) {
-  if (numBytes >= 8) {
+  if (numBytes >= 9) {
     union { byte b[4]; float f; } dist, rot;
-    for (int i = 0; i < 4; i++) dist.b[i] = Wire.read();
-    for (int i = 0; i < 4; i++) rot.b[i] = Wire.read();
+
+    // Read command byte
+    if (Wire.available()) {
+      command = Wire.read();
+    }
+
+    // Read 4 bytes for distance
+    for (int i = 0; i < 4; i++) {
+      if (Wire.available()) dist.b[i] = Wire.read();
+    }
+
+    // Read 4 bytes for rotation
+    for (int i = 0; i < 4; i++) {
+      if (Wire.available()) rot.b[i] = Wire.read();
+    }
+
     received_distance = dist.f;
     received_rotation = rot.f;
     received = true;
+
+    // Decide what to do based on the command
+    switch (command) {
+      case 0x00: // optional
+        continuous_turn = true;
+        Serial.println("Resuming continuous rotation");
+        break;
+
+      case 0x01:
+        continuous_turn = false;
+        Serial.println("Stopping continuous rotation");
+        break;
+
+      case 0x02:
+        continuous_turn = false;
+        desired_pos_xy[0] = received_rotation;
+        desired_pos_xy[1] = received_distance;
+        Serial.println("Switching to Pi-guided movement");
+        break;
+
+      case 0x03:
+        Serial.println("Adjusting left turn");
+        desired_pos_xy[0] = -90;
+        break;
+
+      case 0x04:
+        Serial.println("Adjusting right turn");
+        desired_pos_xy[0] = 90;
+        break;
+    }
   }
 }
 
@@ -39,6 +89,7 @@ void onRequestEvent() {
   union { byte b[4]; float f; } dist, rot;
   dist.f = received_distance;
   rot.f = received_rotation;
+
   for (int i = 0; i < 4; i++) Wire.write(dist.b[i]);
   for (int i = 0; i < 4; i++) Wire.write(rot.b[i]);
 }
