12a8a5b71a597ee5caf3bd122b21d828e80de65e
Jason Huang <huangjason1229@gmail.com>
2025-11-21 13:28:36 -0700

    sadfasd

--- final_demo/main.py
+++ final_demo/main.py
@@ -130,9 +130,7 @@ while True:
                 firstfind = 1
                 time.sleep(0.5)
             if (change):
-                if abs(angle) > 4:
-                    print(f"angle correct {angle}")
-                    send_command(0,0, "turn")
+
                 if not (abs(angle) < 0.02 and abs(distance_val) < 4) and abs(angle) < 4 and abs(distance_val) < 52:
                     print(f"angle 1 is {angle} \n")
                     print(f"angle 2 is {angle2} \n")
