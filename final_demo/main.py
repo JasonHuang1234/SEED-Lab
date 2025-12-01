aaf0aaac0ee938c6be615870dd76a9e95f675f33
Jason Huang <huangjason1229@gmail.com>
2025-12-01 12:38:02 -0700

    asdfas

--- final_demo/main.py
+++ final_demo/main.py
@@ -133,8 +133,11 @@ while True:
                 send_command(0, 0, "stop")
                 firstfind = 1
                 time.sleep(0.1)
+            
+
+
             if (change):
-                if not (abs(angle) < 0.5 and abs(distance_val) < 4) and abs(angle) < 10 and abs(distance_val) < 60:
+                if not (abs(angle) < 0.5 and abs(distance_val) < 4) and abs(angle) < 30 and abs(distance_val) < 60:
                     print(f"angle 1 is {angle} \n")
                     print(f"angle 2 is {angle2} \n")
                     print(f"distance in inches from marker is {distance_val} \n")
