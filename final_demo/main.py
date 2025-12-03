083f3de94b61dc9fe35d2652394f2fd9748f0c93
Jason Huang <huangjason1229@gmail.com>
2025-12-03 12:48:47 -0700

    angle

--- final_demo/main.py
+++ final_demo/main.py
@@ -141,10 +141,10 @@ while True:
                 time.sleep(1)
             if (change):
                 if abs(angle) > 10:
+                    print(f"angle sent is {angle}")
                     send_command(0, angle, "angle")
                 if not (abs(angle) < 0.5 and abs(distance_val) < 4) and abs(angle) <= 10 and abs(distance_val) < 50:
                     print(f"angle 1 is {angle} \n")
-                    print(f"angle 2 is {angle2} \n")
                     print(f"distance in inches from marker is {distance_val} \n")
                     time.sleep(0.1)
                     send_command(distance_val,angle, "control")
