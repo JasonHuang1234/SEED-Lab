67175353fa5f810dd3ceac5e6189a2e76a4e81ed
Kiera <kieracrawford2005@gmail.com>
2025-12-04 13:12:57 -0700

    dist change

--- final_demo/main.py
+++ final_demo/main.py
@@ -140,7 +140,7 @@ while True:
                 firstfind = 1
                 time.sleep(0.1)
             if (change):
-                if not (abs(angle) < 0.5 and abs(distance_val) < 4) and abs(angle) < 20 and abs(distance_val) < 50:
+                if not (abs(angle) < 0.5 and abs(distance_val) < 4) and abs(angle) < 16 and abs(distance_val) < 55:
                     print(f"angle 1 is {angle} \n")
                     print(f"angle 2 is {angle2} \n")
                     print(f"distance in inches from marker is {distance_val} \n")
