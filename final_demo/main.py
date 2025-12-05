4e1516b6f9eb942b9c30c0a978f788a4295e985b
Kiera <kieracrawford2005@gmail.com>
2025-12-05 10:22:03 -0700

    error change

--- final_demo/main.py
+++ final_demo/main.py
@@ -140,7 +140,7 @@ while True:
                 firstfind = 1
                 time.sleep(0.1)
             if (change):
-                if not (abs(angle) < 0.5 and abs(distance_val) < 4) and abs(angle) < 20 and abs(distance_val) < 55:
+                if not (abs(angle) < 4 and abs(distance_val) < 4) and abs(angle) < 20 and abs(distance_val) < 55:
                     print(f"angle 1 is {angle} \n")
                     print(f"angle 2 is {angle2} \n")
                     print(f"distance in inches from marker is {distance_val} \n")
@@ -157,7 +157,7 @@ while True:
         if change and firstfind == 0:
             print("No markers found")
             send_command(0,0, "turn")
-    if abs(angle) < 0.5 and abs(distance_val) < 4 and firstfind == 1 and turned == 0: #and direction is less than a given error
+    if abs(angle) < 4 and abs(distance_val) < 4 and firstfind == 1 and turned == 0: #and direction is less than a given error
         time.sleep(0.1)
         send_command(0, 0, "stop")
         if ids is not None:
